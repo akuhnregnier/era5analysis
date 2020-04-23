28fb99da6bcb8f9ee6c7c75d5ffca29bfc3c1526
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2020-04-23 13:16:44 +0100

    Managed version using setuptools-scm. Added configuration files and restructured package.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-import os
-import re
 
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
     readme = f.read()
@@ -10,32 +8,17 @@ with open("README.md", "r") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-
-def read(*parts):
-    with open(os.path.join(here, *parts), "r") as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-setuptools.setup(
+setup(
     name="era5analysis",
-    version=find_version("era5analysis", "__init__.py"),
     author="Alexander Kuhn-Regnier",
     author_email="ahf.kuhnregnier@gmail.com",
     description="Download and analyse ERA5 data from the Climate Data Store.",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/akuhnregnier/era5analysis",
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -44,4 +27,7 @@ setuptools.setup(
     python_requires=">=3.6",
     include_package_data=True,
     # install_requires=requirements,
+    extras_require={"test": ["pytest>=5.4", "pytest-cov>=2.8"]},
+    setup_requires=["setuptools-scm"],
+    use_scm_version=dict(write_to="src/era5analysis/_version.py"),
 )
--- src/era5analysis/__init__.py
+++ src/era5analysis/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from ._version import version as __version__
 from .data import *
 from .era5_download import *
 from .era5_tables import *
 from .logging_config import *
 from .processing_workers import *
 
-__version__ = "0.2.0"
+del _version
